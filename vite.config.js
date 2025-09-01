18935859fa3f5a1974c06421f8b376fd3b0c7aa3
Ayush <ayushsunilupase999@gmail.com>
2025-09-01 09:53:32 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  server: {
+    allowedHosts: ['2yf876-5173.csb.app'] // ✅ allow your sandbox host
+  }
 })
